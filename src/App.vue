3b3ae76b40ee538e15aa22fafb7ee14281493dbe
-Haru- <52265509+KenPrz@users.noreply.github.com>
2024-05-24 00:15:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import NavBar from './components/Navbar.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="px-5">
+      <NavBar />
+    </div>
+    <router-view />
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+<template>
+    <nav class="py-3 flex items-center justify-center md:justify-between">
+        <div class="md:flex hidden items-center">
+            <router-link class="flex items-center" to="/">
+                <img src="../assets/profile.jpg" alt="logo" class="h-10 w-10 rounded-full" />
+                <h1 class="text-xl font-bold ml-2">KenPrz</h1>
+            </router-link>
+        </div>
+        <div class="space-x-5 md:space-x-10">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+            <router-link to="/projects">Projects</router-link>
+            <router-link to="/contact">Say Hi!</router-link>
+        </div>
+    </nav>
+</template>
+<script>
+</script>
